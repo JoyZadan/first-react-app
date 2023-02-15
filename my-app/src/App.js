5d8fa8ad2562f64dc2b275982ae677747f34a764
JoyZadan <joyzadan.learning@gmail.com>
2023-02-15 13:08:10 +0000

    Add css files in components folder, amended App.js and added App.module.css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 // import FunctionalGreeting from "./components/FunctionalGreeting";
-import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
+// import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
+
+// function App() {
+//     return (
+//         <div className="App">
+//             {/* This component is totally independent and won't have
+//             any other content except what it renders itself, so it
+//             doesn't need a closing tag, we just close it with a space slash */}
+//             {/* <FunctionalGreeting /> */}
+//             <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" />
+//         </div>
+//     );
+// }
+
+// export default App;
+import css from './App.module.css';
+import Sidebar from "./components/Sidebar";
 
 function App() {
-    return (
-        <div className="App">
-            {/* This component is totally independent and won't have
-            any other content except what it renders itself, so it
-            doesn't need a closing tag, we just close it with a space slash */}
-            {/* <FunctionalGreeting /> */}
-            <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" />
-        </div>
-    );
+  return (
+    <div className={css.App}>
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
--- my-app/src/components/FunctionalGreeting.js
+++ my-app/src/components/FunctionalGreeting.js
@@ -3,7 +3,7 @@
 // to create all the HTML elements this component will render
 // this line is not required in latest versions of React
 // but makes this component backwards compatible with earlier versions
-import React from "react";
+// import React from "react";
 
 // vanilla JS function
 // function FunctionalGreeting() {
@@ -11,8 +11,9 @@ import React from "react";
 // }
 
 // using arrow function
-const FunctionalGreeting = () => <h1>Hello from React!</h1>
+// const FunctionalGreeting = () => <h1>Hello from React!</h1>
+
 
 
 // exports the function to include the component with the rest of the application
-export default FunctionalGreeting;
+// export default FunctionalGreeting;
